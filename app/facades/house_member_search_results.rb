bfc674b6f77b00ab5b20c44bb8250db31fe9b259
Andrew Johnson <manilda7@gmail.com>
2019-09-06 10:50:40 -0600

    Adds better testing and starts building declaritive program

---
+++ app/facades/house_member_search_results.rb
@@ -0,0 +1,13 @@
+class HouseMemberSearchResults
+  def initialize(house)
+    @house = house
+  end
+
+  def house_member_count
+    house_member.count
+  end
+
+  def house_member 
+
+  end
+end
--- spec/features/user_can_select_house_spec.rb
+++ spec/features/user_can_select_house_spec.rb
@@ -5,8 +5,17 @@ describe "As a user" do
     describe "I then click on 'Get Members', then my my path should be '/search' with 'house=greyjoy' in parameters" do
       it "I should see a message '7 Members' and a list of the members with name and id" do
         visit '/'
+        expect(current_path).to eq(root_path)
+        select "Greyjoy", from: :house
+        click_on "Get Members"
 
-        
+        expect(current_path).to eq(search_path)
+        expect(page).to have_content("7 Members")
+        expect(page).to have_css(".member", count: 7)
+        within(first(".member")) do
+          expect(page).to have_css(".name")
+          expect(page).to have_css(".id")
+        end
       end
     end
   end
