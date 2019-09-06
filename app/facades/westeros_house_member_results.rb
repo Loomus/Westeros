0528b38dc25c37c12324822e6bedac3bea521202
Andrew Johnson <manilda7@gmail.com>
2019-09-06 11:39:31 -0600

    Adds figaro, working through getting faraday object

--- app/facades/westeros_house_member_results.rb
+++ app/facades/westeros_house_member_results.rb
@@ -8,6 +8,14 @@ class WesterosHouseMemberResults
   end
 
   def house_member
+    conn = Faraday.new(url: "https://westerosapi.herokuapp.com") do |faraday|
+      # faraday.headers["api_key"] = ENV['Westeros_key']
+      # faraday.request
+      faraday.adapter Faraday.default_adapter
+      binding.pry
+    end
+
+    response = conn.get("/api/v1/house/#{house}")
 
   end
 end
